cfe6634ecf422f1bc11938f90136dd2889162444
elliskelly99 <71240980+elliskelly99@users.noreply.github.com>
2024-07-18 13:32:07 -0400

    - Bug fix where font-Awesome packages were not was not appearing - Display AirBnb Logo

---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,9 @@
+import { faAirbnb } from "@fortawesome/free-brands-svg-icons";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons"
+
+
+//Anytime we need to import an new icon from fortawesome we come here
+export const fontAwesomeIcons: IconDefinition[] = [
+  faAirbnb
+
+]
