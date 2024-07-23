cc5900b34886913d14d64ab5246e0679ce75f790
elliskelly99 <71240980+elliskelly99@users.noreply.github.com>
2024-07-23 16:24:42 -0400

    - Added toast module, created grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,18 @@ import { FaIconLibrary, FontAwesomeModule } from "@fortawesome/angular-fontaweso
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
 import {NavbarComponent} from "./layout/navbar/navbar.component";
 import { FooterComponent } from './layout/footer/footer.component';
+import {ToastModule} from "primeng/toast";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent,FooterComponent],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent,FooterComponent, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   faIconLibrary:FaIconLibrary = inject(FaIconLibrary);
+  isListingView:Boolean = true;
 
   ngOnInit(): void {
     this.initFontAwesome();
